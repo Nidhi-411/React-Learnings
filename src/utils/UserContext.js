4cf9ef43648d8aa269f555592c391f4255038381
Nidhi Yadav <shub01690@gmail.com>
2024-05-02 12:43:12 +0530

    context concept

--- src/utils/UserContext.js
+++ src/utils/UserContext.js
@@ -1,6 +1,6 @@
 import { createContext } from "react";
-const userContext =  createContext({
+const UserContext =  createContext({
  
     loggedInUser : "Default user"
 })
-export default userContext;
+export default UserContext;
